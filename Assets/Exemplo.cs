a3e37f41950a573e074f58ff65d1cf2833bad6ac
felipe_brito89 <brito.feh@gmail.com>
2020-08-07 10:19:26 -0300

    added 3d models
    added 3d models and scene

---
+++ Assets/Exemplo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exemplo : MonoBehaviour
+{
+    public Text text;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "texto";
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            rb.AddForce(transform.up * 10);
+        }
+    }
+}
